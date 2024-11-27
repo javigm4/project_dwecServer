a465ac807329b9649cf1fe1aae24bb4d3d3dfb2c
javigm4 <javierguerreromontero1@gmail.com>
2024-11-27 10:32:46 +0100

    terminado ya

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,12 @@
-const url = "http://localhost:3000/posts";
+const url1 = "http://localhost:3000/posts";
+const url2 = "http://localhost:3000/comments";
 
 const options = {
   method: "GET",
 };
 
-fetch(url, options)
+//posts
+fetch(url1, options)
   .then((response) => response.json())
   .then((data) => {
     console.log(data); //probar que funciona
@@ -13,9 +15,26 @@ fetch(url, options)
 
     data.forEach((dataLi) => {
       let li = document.createElement("li");
-      li.textContent = dataLi.title;
+      li.textContent = `${dataLi.title} , ${dataLi.author}`;
       ul.appendChild(li);
     });
 
-    document.appendChild(ul);
+    document.body.appendChild(ul);
+  });
+
+//comments
+fetch(url2, options)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data); //probar que funciona
+
+    const ul = document.createElement("ul");
+
+    data.forEach((dataLi) => {
+      let li = document.createElement("li");
+      li.textContent = `${dataLi.body} , ${dataLi.postId}`;
+      ul.appendChild(li);
+    });
+
+    document.body.appendChild(ul);
   });
