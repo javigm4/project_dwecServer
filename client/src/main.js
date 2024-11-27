10dacedefc2964f77f8ddfd09185ef5aff7acaad
javigm4 <javierguerreromontero1@gmail.com>
2024-11-27 09:58:08 +0100

    nose a ver

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,4 @@
-import "./style.css";
-
-url = "db.json";
+const url = "http://localhost:3000/posts";
 
 const options = {
   method: "GET",
@@ -8,4 +6,16 @@ const options = {
 
 fetch(url, options)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    console.log(data); //probar que funciona
+
+    const ul = document.createElement("ul");
+
+    data.forEach((dataLi) => {
+      let li = document.createElement("li");
+      li.textContent = dataLi.title;
+      ul.appendChild(li);
+    });
+
+    document.appendChild(ul);
+  });
